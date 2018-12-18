571d97d5e0d006da10a3fd957df21f58e2d2ab24
unknown <you@example.com>
2018-12-18 15:50:43 +0800

    "12.18 15:50"

--- js/zhuce.js
+++ js/zhuce.js
@@ -1,6 +1,6 @@
 //账号是否验证过
-var accountIsChecked = false;
-var accountIsOK = false;
+var username = false;
+var phoneId = false;
 var passwdIsOK = false;
 var confirmpwdIsOK = false;
 var phoneIsOK = false;
@@ -8,17 +8,17 @@ var phoneIsOK = false;
 
 $('#uname').blur(function(){ 
     var zh=$('#uname').val();
-    var zhpd=/^[0-9a-zA-Z]{6,20}$/;
+    var zhpd=/^[\w\u4e00-\u9fa5]{3,12}$/;
     if(zh==''){
         $('#uname-text').text('用户名不能为空');
         $('#uname-text').css('color','red');
         return false;
     }else if(!zhpd.test(zh)){
-        $("#uname-text").text('请输入您的用户名,长度为6-20位,不能含有特殊字符');
+        $("#uname-text").text('请输入您的用户名,长度为3-12位,不能含有特殊字符');
         return false;
     }else{
         $("#uname-text").text("用户名格式正确");
-        accountIsChecked=true;
+        username=true;
         // console.log(accountIsChecked);
         return true;
         
@@ -39,6 +39,7 @@ $("#tl").blur(function(){
         return false;
     }else{
         $("#pdtl").text("手机号码格式正确");
+        phoneId=true;
         return true;
     }
 });
@@ -57,6 +58,7 @@ $("#pass").blur(function(){
         return false;
     }else{
         $("#pdpass1").text("密码格式正确");
+        passwdIsOK=true;
         return true;
     }
 });
@@ -69,7 +71,7 @@ $("#pass").blur(function(){
  */
 
 $('#pass').keyup(function () { 
-    var strongRegex = new RegExp("^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\w).*$", "g"); 
+    var strongRegex = new RegExp("^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$", "g"); 
     var mediumRegex = new RegExp("^(?=.{7,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$", "g"); 
     var enoughRegex = new RegExp("(?=.{6,}).*", "g"); 
     
@@ -81,9 +83,9 @@ $('#pass').keyup(function () {
             //密码为八位及以上并且字母数字特殊字符三项都包括,强度最强 
     } 
     else if (mediumRegex.test($('#pass').val())) { 
-        $('#level').removeClass('pw-medium'); 
-        $('#level').removeClass('pw-strong'); 
-        $('#level').addClass('pw-medium'); 
+        $('#low').removeClass('pw-defule'); 
+        $('#strong').removeClass('pw-strong'); 
+        $('#medium').addClass('pw-medium'); 
             //密码为七位及以上并且字母、数字、特殊字符三项中有两项，强度是中等 
     } 
     else if(enoughRegex.test($('#pass').val())){ 
@@ -91,6 +93,71 @@ $('#pass').keyup(function () {
         $('#strong').removeClass('pw-strong'); 
         $('#low').addClass('pw-defule'); 
             //如果密码为6为及以下，就算字母、数字、特殊字符三项都包括，强度也是弱的 
-    } 
+    } else{
+        $('#medium').removeClass('pw-medium'); 
+        $('#strong').removeClass('pw-strong'); 
+        $('#low').removeClass('pw-defule'); 
+    }
     return true; 
 }); 
+
+/**
+ * 两次密码判断相等不相等
+ */
+$('#pass2').blur(function(){
+    var pass2 = $("#pass2").val();
+    if(!$("#pass").val()==pass2){
+        $("#pass3").text("两次密码不相同");
+        $("#pass3").css("color","red");
+        return false;
+    }else if($("#pass").val()==pass2){
+        $("#pass3").text("两次密码相同");
+        $("#pass3").css("color","red");
+        confirmpwdIsOK=true;
+        return true;
+    }else{
+        $("#pass3").text("请再次输入密码");
+    }
+});
+//按钮提交
+$('#zcbtn').click(function(){
+    if(username && phoneId && passwdIsOK &&confirmpwdIsOK ==true){
+        let uname=;
+        let phone=;
+        let 
+        $.ajax({
+            url:"",
+            type:'post',
+            data:{},
+            dataType:'josn',
+            contentType: 'application/json;charset=UTF-8',
+            success: function (data){
+
+            },
+            error:function(error){
+                console.log(error);
+            }
+        });
+    }else{
+        alert("注册信息有误,请重新填写");
+        $('#uname').val('');
+        $("#tl").val('');
+        $("#pass").val('');
+        $("#pass2").val('');
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
